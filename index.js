e94059eede41aaabe6f99a5ec8fcf99201d5d13c
Luke Wheldale <lukewheldale@gmail.com>
2024-07-30 12:12:10 +0930

    server set up init

---
+++ index.js
@@ -0,0 +1,8 @@
+const axios = require("axios");
+
+let http = require("http");
+
+http.createServer(function (req, res) {
+	res.writeHead(200, { "Content-Type": "text/html" });
+	res.end("Hello World!");
+}).listen(8080);
